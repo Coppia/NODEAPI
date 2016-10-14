44fc56f0cd29aba564969cefbd33a31e03a37849
talmasc <eric.l.hillenbrand@gmail.com>
2016-10-14 08:48:42 -0700

    updated

--- config/config.js
+++ config/config.js
@@ -7,11 +7,17 @@ module.exports = {
             port: 7002
     },
     database: {
-        host: 'coppia-mysql.c2nenncbrekn.us-west-2.rds.amazonaws.com',
+        host: 'us-cdbr-iron-east-04.cleardb.net',
         port: 3306,
-        database: 'coppia',
-        user: 'coppia_admin',
-        password: 'squid2009'
+        database: 'heroku_784e2906e8bab8d',
+        user: 'bf5e988a764f51',
+        password: 'e855508e'
+
+        // host: 'coppia-mysql.c2nenncbrekn.us-west-2.rds.amazonaws.com',
+        // port: 3306,
+        // database: 'coppia',
+        // user: 'coppia_admin',
+        // password: 'squid2009'
     },
     secret: {
         key: 'a720496337f7bf3e27a6b763e34a7662f1961709daf7e506c590c6ba502bd783'
