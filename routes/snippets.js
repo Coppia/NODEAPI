dc31d98b7fba14178bf847c667de889f76514804
talmasc <eric.l.hillenbrand@gmail.com>
2016-10-31 14:40:49 -0700

    updated snippets for user image

--- routes/snippets.js
+++ routes/snippets.js
@@ -46,9 +46,11 @@ router.get('/', function(req, res, next) {
                                     snippets.text, 
                                     snippets.interview_id, 
                                     CONCAT(create_users.first_name, ' ', create_users.last_name) as created_by,
-                                    snippets.create_datetime as created_date, 
+                                    snippets.create_datetime,
+                                    create_users.image_link as created_image_link, 
                                     CONCAT(update_users.first_name, ' ', update_users.last_name) as updated_by,
-                                    snippets.update_datetime as updated_date
+                                    snippets.update_datetime,
+                                    update_users.image_link as updated_image_link
                             FROM    snippets
                             JOIN	users as create_users
                                 ON	snippets.create_user = create_users.id 
@@ -88,9 +90,11 @@ router.get('/:snippet_id', function(req, res, next) {
                                     snippets.text, 
                                     snippets.interview_id, 
                                     CONCAT(create_users.first_name, ' ', create_users.last_name) as created_by,
-                                    snippets.create_datetime as created_date, 
+                                    snippets.create_datetime,
+                                    create_users.image_link as created_image_link, 
                                     CONCAT(update_users.first_name, ' ', update_users.last_name) as updated_by,
-                                    snippets.update_datetime as updated_date
+                                    snippets.update_datetime,
+                                    update_users.image_link as updated_image_link
                             FROM    snippets
                             JOIN	users as create_users
                                 ON	snippets.create_user = create_users.id 
