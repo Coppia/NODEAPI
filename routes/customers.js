4138a7e41b92e42eb30374791efabb157327b1ba
talmasc <eric.l.hillenbrand@gmail.com>
2016-11-14 09:39:05 +0900

    checking database for customer email before calling clearbit

--- routes/customers.js
+++ routes/customers.js
@@ -166,49 +166,121 @@ router.get('/lookup/:email_address', function(req, res, next) {
         var email_address = request.email_address;
 
         if (validator.isEmail(email_address)) {
-            clearbit.Enrichment.find({email: email_address, stream: true})
-                .then(function (result) {
-                    var fullname = result.person.name.fullName;
-                    var firstname = result.person.name.givenName;
-                    var lastname = result.person.name.familyName;
-                    var avatar = result.person.avatar;
-                    var email = result.person.email;
-                    var location = result.person.location;
-                    var domain = result.person.employment.domain;
-                    var company_name = result.person.employment.name;
-                    var title = result.person.employment.title;
-                    var role = result.person.employment.role;
-                    
-                    res.json(
-                        {
-                            "success" : "true",
-                            "full_name" : fullname,
-                            "last_name" : lastname,
-                            "first_name" : firstname,
-                            "email" : email,
-                            "image_link" : avatar,
-                            "location" : location,
-                            "domain" : domain,
-                            "company_name" : company_name,
-                            "title" : title,
-                            "role" : role
-                        });
-                    //console.log(person);
-                })
-                .catch(clearbit.Enrichment.NotFoundError, function (err) {
-                    // Email address could not be found
-                    res.json(
-                        {
-                            "success" : "false",
-                            "customer_id":customer_id
-                        });
-                    console.log(err);
-                })
-                .catch(function (err) {
-                    console.error('Clearbit Connection Error: ', err);
+
+            // see if the customer exists in the database before calling the clearbit API
+            pool.getConnection(function(err, conn) {
+                if (err) {
+                    console.error('SQL Connection error: ', err);
                     return next(err);
                 }
-            );
+                else {
+                    conn.query(`SELECT  customers.id, 
+                                        customers.first_name, 
+                                        customers.last_name, 
+                                        customers.email, 
+                                        customers.image_link, 
+                                        customers.title, 
+                                        customers.company_name, 
+                                        CONCAT(create_users.first_name, ' ', create_users.last_name) as created_by,
+                                        customers.create_datetime as created_datetime,
+                                        create_users.image_link as created_image_link, 
+                                        CONCAT(update_users.first_name, ' ', update_users.last_name) as updated_by,
+                                        customers.update_datetime as updated_datetime,
+                                        update_users.image_link as updated_image_link
+                                FROM    customers
+                                JOIN	users as create_users
+                                    ON	customers.create_user = create_users.id
+                                JOIN	users as update_users
+                                    ON	customers.update_user = update_users.id WHERE customers.id = ?`, customer_id, function(err, rows, fields) {
+                        conn.release();
+
+                        if (err) {
+                            console.error('SQL Error: ', err);
+                            return next(err);
+                        }
+
+                        if (result.affectedRows > 0) {
+                            var id = rows[0].id,
+                                first_name = rows[0].first_name,
+                                last_name = rows[0].last_name,
+                                email = rows[0].email,
+                                image_link = rows[0].image_link, 
+                                title = rows[0].title, 
+                                company_name = rows[0].company_name, 
+                                created_by = rows[0].created_by,
+                                created_datetime = rows[0].created_datetime,
+                                created_image_link = rows[0].created_image_link, 
+                                updated_by = rows[0].updated_by,
+                                updated_datetime = rows[0].updated_datetime,
+                                updated_image_link = rows[0].updated_image_link
+
+                        
+                            res.json(
+                                {
+                                    "id" : id,
+                                    "first_name" : first_name,
+                                    "last_name" : last_name,
+                                    "email" : email,
+                                    "image_link" : image_link, 
+                                    "title" : title, 
+                                    "company_name" : company_name, 
+                                    "created_by" : created_by,
+                                    "created_datetime" : created_datetime,
+                                    "created_image_link" : created_image_link, 
+                                    "updated_by" : updated_by,
+                                    "updated_datetime" : updated_datetime,
+                                    "updated_image_link" : updated_image_link
+                                }
+                            );
+
+                        } else {
+                            clearbit.Enrichment.find({email: email_address, stream: true})
+                                .then(function (result) {
+                                    var fullname = result.person.name.fullName;
+                                    var firstname = result.person.name.givenName;
+                                    var lastname = result.person.name.familyName;
+                                    var avatar = result.person.avatar;
+                                    var email = result.person.email;
+                                    var location = result.person.location;
+                                    var domain = result.person.employment.domain;
+                                    var company_name = result.person.employment.name;
+                                    var title = result.person.employment.title;
+                                    var role = result.person.employment.role;
+                                    
+                                    res.json(
+                                        {
+                                            "success" : "true",
+                                            "full_name" : fullname,
+                                            "last_name" : lastname,
+                                            "first_name" : firstname,
+                                            "email" : email,
+                                            "image_link" : avatar,
+                                            "location" : location,
+                                            "domain" : domain,
+                                            "company_name" : company_name,
+                                            "title" : title,
+                                            "role" : role
+                                        });
+                                    //console.log(person);
+                                })
+                                .catch(clearbit.Enrichment.NotFoundError, function (err) {
+                                    // Email address could not be found
+                                    res.json(
+                                        {
+                                            "success" : "false",
+                                            "customer_id":customer_id
+                                        });
+                                    console.log(err);
+                                })
+                                .catch(function (err) {
+                                    console.error('Clearbit Connection Error: ', err);
+                                    return next(err);
+                                }
+                            );
+                        }
+                    });
+                }
+            });
         } else {
             res.json(
                 {
